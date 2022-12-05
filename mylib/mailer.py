7d6070bb5688dc99ee7ba00954ce6113c4dd582c
Jamshid-Ganiev <84252587+Jamshid-Ganiev@users.noreply.github.com>
2022-12-05 14:01:53 +0900

    Update mailer.py

--- mylib/mailer.py
+++ mylib/mailer.py
@@ -1,4 +1,6 @@
 import smtplib
+from twilio.rest import Client
+import config
 
 class Mailer:
 
@@ -7,20 +9,38 @@ class Mailer:
 
     """
     def __init__(self):
-        # WRITE THE SENDER'S email address here:
+        # sender's email address
         self.EMAIL = ""
-        #the password for the email address || Create an app inside your google acount and write tha google generated password here:
+        # create an app in you google account and enter the password of the app here 
         self.PASS = ""
 
-    def send(self, mail):
+    def send(self, mail_list):
+        for worker_email in mail_list:
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+                connection.starttls(),
+                connection.login(user=self.EMAIL, password=self.PASS),
+                text = f'[OVER {config.LIMIT}]People Limit exceeded in the building!',
+                connection.sendmail(
+                    from_addr=self.EMAIL,
+                    to_addrs=worker_email,
+                    msg = "Subject:'ALERT! ||by GIGA TEAM':.\n\n {}.".format(text)
+                )
+            
+class SMSMailer:
+    def __init__(self):
+        # the given number in your twilio account
+        self.number =''
+        self.account_sid = ""
+        self.auth_token = ""
+    
+    def send(self, phone_number):
 
+        client = Client(self.account_sid, self.auth_token)
 
-        with smtplib.SMTP("smtp.gmail.com") as connection:
-            connection.starttls()
-            connection.login(user=self.EMAIL, password=self.PASS)
-            text = 'People Limit exceeded in the building!',
-            connection.sendmail(
-                from_addr=self.EMAIL,
-                to_addrs=mail,
-                msg = "Subject:'ALERT! ||by GIGA TEAM':.\n\n {}.".format(text)
-            )
+        message = client.messages.create(
+            body="People Limit exceeded in the building!",
+            from_=self.number,
+            to=phone_number
+        )
+
+        print(message.status)
